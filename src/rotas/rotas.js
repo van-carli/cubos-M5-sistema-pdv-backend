d7eeff616018a76ebc9291e5a97deb7e5c63069a
Vanessa Carli <van_carli@hotmail.com>
2023-11-24 12:08:35 -0300

    implementação do login do usuário, do arquivo de autenticação e de autorização e  alterações no arquivo .env e

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const { listarCategorias } = require("../controladores/categoria");
+const verificarCadastro = require("../intermediarios/aut_usuario");
+const userId = require('../intermediarios/autorizacao');
+const { login } = require('../controladores/usuario');
 
 const rotas = express();
+
+rotas.post('/login', login);
+
+rotas.use(verificarCadastro);
+rotas.use(userId);
 
 rotas.get("/categoria", listarCategorias);
 
