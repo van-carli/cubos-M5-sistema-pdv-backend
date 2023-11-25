8cead83ad43c1b4557a4af8de3b94e89cd4f1777
Emily Rizo <emilyrizo@gmail.com>
2023-11-25 00:47:31 -0300

    Primeiro commit da Emily, com algumas alterações

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,12 @@
 const knex = require("knex")({
-  client: process.env.DB_CLIENT,
+  client: 'pg',
   connection: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
+    ssl: { rejectUnauthorized: false }
   },
 });
 
--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -2,10 +2,13 @@ const express = require("express");
 const { listarCategorias } = require("../controladores/categoria");
 const verificarCadastro = require("../intermediarios/aut_usuario");
 const userId = require('../intermediarios/autorizacao');
-const { login } = require('../controladores/usuario');
+const { login, cadastrarUsuario } = require('../controladores/usuario');
+const validarRequisicao = require("../intermediarios/validarRequisicao");
+const usuarioSchema = require("../schemas/schemaUsuario");
 
 const rotas = express();
 
+rotas.post('/usuario', validarRequisicao(usuarioSchema), cadastrarUsuario)
 rotas.post('/login', login);
 
 rotas.use(verificarCadastro);
