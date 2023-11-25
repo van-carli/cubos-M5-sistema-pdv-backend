1a520a0efa544a04e78ad284ad7c0e0f7d8c1842
Vanessa Carli <van_carli@hotmail.com>
2023-11-25 14:52:47 -0300

    criação da autenticação, reorganização do endpoint login, criação do loginSchema

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const { listarCategorias } = require("../controladores/categoria");
-const verificarCadastro = require("../intermediarios/aut_usuario");
 const userId = require('../intermediarios/autorizacao');
-const { login, cadastrarUsuario } = require('../controladores/usuario');
+const { cadastrarUsuario } = require('../controladores/usuario');
+const login = require("../controladores/autenticação");
 const validarRequisicao = require("../intermediarios/validarRequisicao");
 const usuarioSchema = require("../schemas/schemaUsuario");
+const filtroLogin = require("../intermediarios/filtroLogin");
+const loginSchema = require("../schemas/loginSchema");
 
 const rotas = express();
 
 rotas.post('/usuario', validarRequisicao(usuarioSchema), cadastrarUsuario)
-rotas.post('/login', login);
+rotas.post('/login', validarRequisicao(loginSchema), login);
 
-rotas.use(verificarCadastro);
+rotas.use(filtroLogin);
 rotas.use(userId);
 
 rotas.get("/categoria", listarCategorias);
