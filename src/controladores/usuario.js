c8dc8c06849efc339a13f7739eb50086856a775d
tais defante <taisadefante@hotmail.com>
2023-11-25 18:55:53 -0300

    alteracao main

--- src/controladores/usuario.js
+++ src/controladores/usuario.js
@@ -9,30 +9,8 @@ const cadastrarUsuario = async (req, res) => {
   try {
     const usuarioEncontrado = await knex("usuarios").where({ email }).first();
 
-<<<<<<< HEAD
     if (usuarioEncontrado) {
       return res.status(400).json("O email já existe");
-=======
-        if (usuarioEncontrado) {
-            return res.status(400).json("O email já existe");
-        }
-
-        const senhaCriptografada = await bcrypt.hash(senha, 10);
-
-        const usuario = await knex('usuarios')
-            .insert({
-                nome,
-                email,
-                senha: senhaCriptografada,
-            }).returning('*');
-
-        const { senha: _, ...usuarioCadastrado } = usuario[0]
-
-        return res.status(201).json(usuarioCadastrado);
-
-    } catch (error) {
-        return res.status(500).json({ mensagem: 'Erro interno do servidor' });
->>>>>>> 417cd523bdccd03fcdabd8e2818d5865651146c0
     }
 
     const senhaCriptografada = await bcrypt.hash(senha, 10);
@@ -53,51 +31,6 @@ const cadastrarUsuario = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-const login = async (req, res) => {
-  const { email, senha } = req.body;
-
-  console.log(email, senha);
-
-  try {
-    if (!email || !senha) {
-      return res
-        .status(400)
-        .json({ mensagem: "Email e senha são obrigatórios." });
-    }
-
-    const usuario = await knex("usuarios").select("*").where("email", email);
-
-    if (usuario.length < 1) {
-      return res.status(404).json({ mensagem: "Usuário não encontrado." });
-    }
-
-    const senhaValida = await bcrypt.compare(senha, usuario[0].senha);
-
-    if (!senhaValida) {
-      return res
-        .status(400)
-        .json({ mensagem: "Usuário e/ou senha inválido(s)." });
-    }
-
-    const token = jwt.sign({ id: usuario[0].id }, process.env.JWT_SECRET_KEY, {
-      expiresIn: process.env.JWT_EXPIRED,
-    });
-
-    const { senha: _, ...usuarioLogado } = usuario[0];
-
-    return res.json({ usuario: usuarioLogado, token });
-  } catch (error) {
-    return res.status(500).json({ mensagem: "Erro interno do servidor" });
-  }
-};
-
 module.exports = {
-  login,
   cadastrarUsuario,
 };
-=======
-module.exports = {
-    cadastrarUsuario
-}
->>>>>>> 417cd523bdccd03fcdabd8e2818d5865651146c0
--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -1,33 +1,19 @@
 const express = require("express");
 const { listarCategorias } = require("../controladores/categoria");
-<<<<<<< HEAD
-const verificarCadastro = require("../intermediarios/aut_usuario");
-const userId = require("../intermediarios/autorizacao");
-const { login, cadastrarUsuario } = require("../controladores/usuario");
-=======
-const userId = require('../intermediarios/autorizacao');
-const { cadastrarUsuario } = require('../controladores/usuario');
-const login = require("../controladores/autenticação");
->>>>>>> 417cd523bdccd03fcdabd8e2818d5865651146c0
+
 const validarRequisicao = require("../intermediarios/validarRequisicao");
 const usuarioSchema = require("../schemas/schemaUsuario");
 const filtroLogin = require("../intermediarios/filtroLogin");
 const loginSchema = require("../schemas/loginSchema");
+const { cadastrarUsuario } = require("../controladores/usuario");
+const login = require("../controladores/autenticação");
 
 const rotas = express();
 rotas.get("/categoria", listarCategorias);
 
-<<<<<<< HEAD
 rotas.post("/usuario", validarRequisicao(usuarioSchema), cadastrarUsuario);
-rotas.post("/login", login);
-=======
-rotas.post('/usuario', validarRequisicao(usuarioSchema), cadastrarUsuario)
-rotas.post('/login', validarRequisicao(loginSchema), login);
->>>>>>> 417cd523bdccd03fcdabd8e2818d5865651146c0
+rotas.post("/login", validarRequisicao(loginSchema), login);
 
 rotas.use(filtroLogin);
-rotas.use(userId);
-
-rotas.get("/categoria", listarCategorias);
 
 module.exports = rotas;
