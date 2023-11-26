f78bab85f1d359fc5523f19b5e6abcf2a0ca2b83
Leticia Woelfer <leticia.woelfer@hotmail.com>
2023-11-26 20:46:27 -0300

    Adicionado tratamento de exceção no controlador categoria

--- src/controladores/categoria.js
+++ src/controladores/categoria.js
@@ -1,9 +1,14 @@
 const knex = require("../conexao");
 
 const listarCategorias = async (req, res) => {
-  const query = await knex("categorias").select("*").from("categorias");
+  try {
+    const query = await knex("categorias").select("*").from("categorias");
 
-  res.status(200).json(query);
+    res.status(200).json(query);
+
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
 };
 
 module.exports = {
