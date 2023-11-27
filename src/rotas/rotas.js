df86eeae54240986eb82c326851f929dc3467dfe
Leticia Woelfer <leticia.woelfer@hotmail.com>
2023-11-26 23:51:11 -0300

    Corrigido o filtroLogin que apresentou erros durante testes

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -7,18 +7,18 @@ const filtroLogin = require("../intermediarios/filtroLogin");
 const autenticacao = require("../intermediarios/autenticacao");
 const usuario_id = require("../intermediarios/autorizacao");
 const loginSchema = require("../schemas/loginSchema");
-const { cadastrarUsuario, editarUsuario } = require("../controladores/usuario");
+const { cadastrarUsuario, editarUsuario, detalharUsuario } = require("../controladores/usuario");
 const login = require("../controladores/autenticação");
 
 const rotas = express();
 rotas.get("/categoria", listarCategorias);
-rotas.get("/usuario", usuario_id, detalharUsuario);
 
 rotas.post("/usuario", validarRequisicao(usuarioSchema), cadastrarUsuario);
 rotas.post("/login", validarRequisicao(loginSchema), login);
 
-//rotas.use(filtroLogin);
+rotas.use(filtroLogin);
 rotas.use(autenticacao);
+rotas.get("/usuario", usuario_id, detalharUsuario);
 rotas.put("/usuario", validarRequisicao(usuarioSchema), editarUsuario);
 
 module.exports = rotas;
