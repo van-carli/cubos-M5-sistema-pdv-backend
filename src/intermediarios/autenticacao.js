b286843a49bdd5f7506b00697d6ffd2299c8a9d7
Iasmin Santos <iasmin.santos.ext@synchro.com.br>
2023-11-26 23:00:02 -0300

    atualizando

--- src/intermediarios/autenticacao.js
+++ src/intermediarios/autenticacao.js
@@ -6,27 +6,30 @@ const filtroLogin = async (req, res, next) => {
     const { authorization } = req.headers
     const token = authorization.split(' ')[1];
 
+
     if (!token) {
         return res.status(401).json({ mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.' })
     }
 
     try {
-        const { id } = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-        const { rows, rowCount } = (await knex('usuarios')
+        const { id } = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    
+
+        const usr = (await knex('usuarios')
             .select('*')
             .where('id', id));
 
-        if (rowCount < 1) {
+        
+        if (usr.length < 1) {
             return res.status(401).json({ mensagem: 'Usuário não encontrado.' })
         }
 
         req.usuario = {
             id,
-            nome: rows[0].nome,
-            email: rows[0].email
+            nome: usr[0].nome,
+            email: usr[0].email
         };
-
         next();
     } catch (error) {
         return res.status(401).json({ mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.' })
